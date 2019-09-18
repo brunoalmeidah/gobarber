63b9e65ee3d332f334a129390466ecebafccf701
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2019-09-17 23:30:10 -0300

    add date validation with date-fns

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -26,6 +27,26 @@ class AppointmentController {
       return res
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
+    }
+    // parseISO cria a string em um objeto date do javascript
+    const hourStart = startOfHour(parseISO(date));
+    // verifica se a data q está sendo enviada é anterior a data atual
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past date are not permitted' });
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        cancelad_at: null,
+        date,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
     }
 
     const appointment = await Appointment.create({
