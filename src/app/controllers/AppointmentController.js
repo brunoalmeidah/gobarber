b8552a241431cd617274d40d49eb72c2ac090b37
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2019-09-18 08:15:42 -0300

    add pagination appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,10 +6,14 @@ import File from '../models/File';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, cancelad_at: null },
       order: ['date'],
       attributes: ['id', 'date'],
+      limit: 2,
+      offset: (page - 1) * 2,
       include: [
         {
           model: User,
