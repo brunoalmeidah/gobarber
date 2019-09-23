0f51a092b1d916aae502b2c8c565010f2a08ff7f
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2019-09-22 21:13:27 -0300

    add envio de email com nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '32448230f368cc',
+    pass: 'd532df6e9e30dc',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
