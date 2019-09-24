30647f76f1bf7c8181f6f1370661b8bd7ccd4602
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2019-09-24 20:30:19 -0300

    add arquivo .env

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: 'd6ba0a30f1555afb849ff574877e3291',
+  secret: process.env.APP_SECRET, // 'd6ba0a30f1555afb849ff574877e3291'
   expiresIn: '7d',
 };
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://85dcc8db25e643a4bd9d2032a7aa597f@sentry.io/1760431',
+  dsn: process.env.SENTRY_DSN, // 'https://85dcc8db25e643a4bd9d2032a7aa597f@sentry.io/1760431'
 };
