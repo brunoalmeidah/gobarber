2fb474447ed791b0630285e1ed70c2d90b485549
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2019-10-24 00:50:42 -0300

    realizado ajustes necessários

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -24,9 +24,16 @@ class ScheduleController {
           [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
         },
       },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
       order: ['date'],
     });
-    return res.json({ appointments });
+    return res.json(appointments);
   }
 }
 
